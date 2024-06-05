369bfeece25281efad1cfd6926cf60086fe14389
mcmilan0 <melinacecilkuehl@gmail.com>
2024-06-05 12:08:12 +0200

    Use FitViewport

--- core/src/main/kotlin/com/libgdx/example/Main.kt
+++ core/src/main/kotlin/com/libgdx/example/Main.kt
@@ -1,8 +1,11 @@
 package com.libgdx.example
 
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.PerspectiveCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
@@ -20,6 +23,12 @@ class Main : KtxGame<KtxScreen>() {
 class FirstScreen : KtxScreen {
     private val image = Texture("logo.png".toInternalFile(), true) // .apply { setFilter(Linear, Linear) }
     private val batch = SpriteBatch()
+    private var camera: Camera = PerspectiveCamera()
+    private var viewport: Viewport = FitViewport(800f, 480f, camera)
+
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height)
+    }
 
     override fun render(delta: Float) {
         clearScreen(red = 0.9f, green = 0.9f, blue = 0.5f)
