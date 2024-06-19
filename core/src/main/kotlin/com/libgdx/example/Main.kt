6138ccf4cc9532c28699f230a63c2ba5cceabe35
wolfsimo <simonxwolf@gmx.de>
2024-06-19 12:44:03 +0200

    Add TicTacToeScreen (WIP)

--- core/src/main/kotlin/com/libgdx/example/Main.kt
+++ core/src/main/kotlin/com/libgdx/example/Main.kt
@@ -24,7 +24,8 @@ import ktx.graphics.use
 class Main : KtxGame<KtxScreen>() {
     override fun create() {
         addScreen(FirstScreen())
-        setScreen<FirstScreen>()
+        addScreen(TicTacToeScreen())
+        setScreen<TicTacToeScreen>()
     }
 }
 
@@ -94,6 +95,31 @@ class SecondScreen : KtxScreen {
 
     override fun render(delta: Float) {
         clearScreen(red = 1f, green = 1f, blue = 1f)
+    }
+
+    override fun dispose() {
+        batch.disposeSafely()
+    }
+}
+
+class TicTacToeScreen : KtxScreen {
+    private val image = Texture("tictactoe.jpg".toInternalFile(), true) // .apply { setFilter(Linear, Linear) }
+    private val batch = SpriteBatch()
+    private var camera: Camera = PerspectiveCamera()
+    private var viewport: Viewport = FitViewport(800f, 480f, camera)
+
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height)
+    }
+
+    override fun render(delta: Float) {
+        clearScreen(red = 1f, green = 1f, blue = 1f)
+        batch.use { batch ->
+            val aspectRatio = 474f / 613f
+            val width = 390f
+            val height = width / aspectRatio
+            batch.draw(image, 130f, 5f, width, height)
+        }
     }
 
     override fun dispose() {
